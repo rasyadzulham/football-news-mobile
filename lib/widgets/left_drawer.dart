f6650bd3adbddb3713dd96b45522e04b9d1d7a8e
rasyadzulham <rasyad2712@gmail.com>
2025-11-12 14:27:50 +0700

    Networking, Authentication, and Integration

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/screens/menu.dart';
 import 'package:football_news/screens/newslist_form.dart';
+import 'package:football_news/screens/news_entry_list.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -61,17 +62,17 @@ class LeftDrawer extends StatelessWidget {
                 ));
           },
         ),
+        // Add this ListTile in your drawer
         ListTile(
-          leading: const Icon(Icons.newspaper_outlined),
-          title: const Text('See Football News'),
-          // Bagian redirection ke halaman melihat news
-          onTap: () {
-            Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => NewsFormPage(),
-                ));
-          },
+            leading: const Icon(Icons.add_reaction_rounded),
+            title: const Text('News List'),
+            onTap: () {
+                // Route to news list page
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const NewsEntryListPage()),
+                );
+            },
         ),
         ],
       ),
